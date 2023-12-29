795359e3b439c49ec655d2b914c940d26a165974
Vasilis Polyzos <bill_polyzos@hotmail.com>
2023-12-29 19:37:34 +0200

    added level up&down popup view , fixed duplicate items in quiz, fixed delay in highlighting answer box , code clean up and some refactoring

--- Kwizzles/DataFactories/QuizDataFactory.swift
+++ Kwizzles/DataFactories/QuizDataFactory.swift
@@ -8,12 +8,12 @@
 import Foundation
 import SwiftUI
 
-class QuizData {
-     var json: QnAResponse
-     var data: [QuizModel]
-     var errorj = QuizJsonData(question: "ERROR", answer: "ERROR", options: nil, points: 0, level: nil)
-     var errorQ: QuizModel
-     
+class QuizDataFactory {
+    var json: QnAResponse
+    var data: [QuizModel]
+    var errorj = QuizJsonData(question: "ERROR", answer: "ERROR", options: nil, points: 0, level: nil)
+    var errorQ: QuizModel
+    
     init() {
         self.json = QuizManager.shared.getDataFromJSON()
         self.data = self.json.quiz.map({ data -> [QuizModel] in
@@ -26,55 +26,74 @@ class QuizData {
         self.errorQ = QuizModel(with: errorj)
     }
     
-    func randomizeQuestions() -> [QuizModel] {
-        var randomQuestions: [QuizModel] = []
-        for index in 0...19 {
-            randomQuestions.append(data.randomElement() ?? errorQ)
-            if(randomQuestions.filter{ $0.question == randomQuestions[index].question }.count > 1) {
-                randomQuestions.append(data.randomElement() ?? errorQ)
-            }
-        }
-        return randomQuestions
+//    func fetchQuestionBasedOnLevel(with level: GameLevels) -> [QuizModel] {
+//        var questions: [QuizModel] = []
+//        let levelArray = data.filter { model in
+//            model.level == level.rawValue
+//        }
+//        
+//        while questions.count < 20 {
+//            var randomIndex = Int(arc4random_uniform(UInt32(levelArray.count)))
+//            while questions.contains(where: { model in
+//                model.question == levelArray[randomIndex].question
+//            }) {
+//                randomIndex = Int(arc4random_uniform(UInt32(levelArray.count)))
+//            }
+//            questions.append(levelArray[randomIndex])
+//        }
+//
+//        return questions
+//    }
+//    
+//    func presentQuestionList(with level: GameLevels) -> [QuizModel] {
+//        var begginersQ: [QuizModel] = []
+//        var intermediateQ: [QuizModel] = []
+//        var proQ: [QuizModel] = []
+//        var masterQ: [QuizModel] = []
+//        var questions: [QuizModel] = []
+//        
+//        begginersQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .beginner))
+//        intermediateQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .intermediate))
+//        proQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .pro))
+//        masterQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .master))
+//        
+//        switch level {
+//        case .beginner:
+//            questions = begginersQ
+//        case .intermediate:
+//            questions = intermediateQ
+//        case .pro:
+//            questions = proQ
+//        case .master:
+//            questions = masterQ
+//        }
+//        
+//        return questions
+//    }
+//    
+    
+    func levelAdjustmentProcess(currentLevel: GameLevels, earnedPointsOfLevel: CGFloat, totalLevelPoints: CGFloat) {
+        
     }
     
-    func fetchQuestionBasedOnLevel(with level: GameLevels) -> [QuizModel] {
-        var questions: [QuizModel] = []
+    func selectRandomQuestionBasedOnLevel(with level: GameLevels) -> QuizModel {
         let levelArray = data.filter { model in
             model.level == level.rawValue
         }
         
-        while questions.count < 20 {
-            let randomIndex = Int(arc4random_uniform(UInt32(levelArray.count)))
-            questions.append(levelArray[randomIndex])
+        var randomIndex = Int(arc4random_uniform(UInt32(levelArray.count)))
+        
+        while levelArray[randomIndex].isUsed {
+            randomIndex = Int(arc4random_uniform(UInt32(levelArray.count)))
         }
-
-        return questions
+        
+        return levelArray[randomIndex]
     }
     
-    func presentQuestionList(with level: GameLevels) -> [QuizModel] {
-        var begginersQ: [QuizModel] = []
-        var intermediateQ: [QuizModel] = []
-        var proQ: [QuizModel] = []
-        var masterQ: [QuizModel] = []
-        var questions: [QuizModel] = []
+    func fetchNextQuestion(with level: GameLevels) -> QuizModel {
+        let question = selectRandomQuestionBasedOnLevel(with: level)
         
-        begginersQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .beginner))
-        intermediateQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .intermediate))
-        proQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .pro))
-        masterQ.append(contentsOf: fetchQuestionBasedOnLevel(with: .master))
-        
-        switch level {
-        case .beginner:
-            questions = begginersQ
-        case .intermediate:
-            questions = intermediateQ
-        case .pro:
-            questions = proQ
-        case .master:
-            questions = masterQ
-        }
-        
-        return questions
+        return question
     }
 }
 
---
+++ Kwizzles/Views/GameScreen/LevelPopUpView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelPopUpView.swift
+//  Kwizzles
+//
+//  Created by Bill on 29/12/23.
+//
+
+import SwiftUI
+
+struct LevelPopUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelPopUpView()
+}
